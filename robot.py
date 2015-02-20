5cbb8a75048546d10bb3eccacfd6228a5a0de82a
Sam Barratt <milktheelephant@gmail.com>
2015-02-20 16:53:11 +0000

    Making variables private

--- robot.py
+++ robot.py
@@ -2,15 +2,15 @@ class Robot:
 
 	def __init__(self): #Constructor
 		pygame.sprite.Sprite.__init__(self)
-		if Robot.image is none:             #What is this meant to be doing?
-			Robot.image = pygame.image.load("robot.png")
+		if Robot.__image is none:             #What is this meant to be doing?
+			Robot.__image = pygame.image.load("robot.png")
 
-		self.image = Robot.image
-		self.name = ""
-		self.points = ""
-		self.rect = Robot.image.get_rect()
-		self.rect.x = 0
-		self.rect.y = 0
+		self.__image = Robot.image
+		self.__name = ""
+		self.__points = ""
+		self.__rect = Robot.__image.get_rect()
+		self.__rect.x = 0
+		self.__rect.y = 0
 
 
     def moveUp(self):   #move robot functions
@@ -22,13 +22,13 @@ class Robot:
     def moveRight(self):  #move robot functions
 
     def returnLocationX():  #Return the X location of the bot
-        return self.rect.x
+        return self.__rect.x
 
     def returnRectY(self):  #Return the Y location of the bot
-        return self.rect.y
+        return self.__rect.y
 
     def returnPoints(self):
-        return self.points
+        return self.__points
 
 
 
