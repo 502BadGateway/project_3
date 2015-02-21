d502bf48ff030ee8bd67bd3a244ede67792c64ec
Adenuga Banjoko <banjokoa@uni.coventry.ac.uk>
2015-02-21 21:30:22 +0000

    Update collectorBot.py

--- collectorBot.py
+++ collectorBot.py
@@ -2,4 +2,15 @@ import robot.py
 
 
 class collectorBot(robot):      #Class for the collector robot. Inherits from the super class robot.
+  def__init__(self):
+    self.score = 0
+    self.treasure = ("London", "Johnasabergg", "New York", "Paris", "Tokyo")
+   
+    if answerinput.touch() in treasure:
+      print ("You've found the Treasure!")
+      score + 1
 
+    else:
+     print ("That is a Trap!...")
+     score - 1
+    print (score)
