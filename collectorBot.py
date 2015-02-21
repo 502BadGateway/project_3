978259e4d84cbedbc2d6a41bbe0dfb232952ff65
Adenuga Banjoko <banjokoa@uni.coventry.ac.uk>
2015-02-21 23:01:27 +0000

    Update collectorBot.py

--- collectorBot.py
+++ collectorBot.py
@@ -1,4 +1,5 @@
 import robot.py
+import simplegui
 #Landmark1 - London
 #Landmark2 - Paris
 #Landmark3 - Johnasaburgg
@@ -39,4 +40,39 @@ class collectorBot(robot):      #Class for the collector robot. Inherits from th
       else:
         print "There are no Traps here!"
         
+# Define global variables
+mins, sec, count, x, y, x1, y2 = 0, 0, 0, 0, 0, 0, 0
+
+# counting tenths of seconds into formatted string A:BC.D
+    def format(t):
+      global count, sec, mins
+      t = count   
+      
+      if (t == 10):
+        sec += 1
+        count = 0
+      if (sec == 60):
+        mins += 1
+        sec = 0
+    def start_timer():
+      global x1, y2
+      if x1-y2 == 0:
+        x1 += 1
+        timer.start()
+
+    def stop_timer():
+      global x, y, x1, y2
+      if x1-y2 == 1:
+        y2 += 1
+        timer.stop()
+        y += 1
+      if count == 0 :
+        x += 1
     
+    def reset_timer():
+      global count, sec, mins, x, y, x1,y2
+      timer.stop()
+      mins, sec, count, x, y,x1,y2 = 0, 0, 0, 0, 0, 0, 0
+    
+    def exit_timer():
+      frame.stop()
