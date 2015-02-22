8bdb0035d9770bc1245199c3c4dc247a5857e456
Dan Beglin <dbeglinuk@gmail.com>
2015-02-22 15:44:55 +0000

    Added basic robot class to test

--- testSort.py
+++ testSort.py
@@ -7,13 +7,37 @@ This, when completed will allow the group to test their own sorting algorithms
 
 """
 
-
-
 import pygame
+
+pygame.init()
 
 class display:
 
 	def __init__(self):
 
+
 		self.displaySort = pygame.display.set_mode((1280, 720))
 		self.background = pygame.image.load(sortBackground)
+		self.size = self.background.get_size()
+
+
+class sortBot:
+
+	def __init__(self):
+	pygame.sprite.Sprite.__init__(self)
+			if sortBot.image is none:             #Loads image dispite some sort of pygame feature to do with Sprites.
+				sortBot.image = pygame.image.load("robot.png")
+
+	self.image = sortBot.image
+	self.name = ""
+	self.points = ""
+	self.rect = self.image.get_rect()
+	self.rect.x = 0
+	self.rect.y = 0
+
+	def returnRectX():
+		return self.rect.x
+
+	def returnRectY():
+		return self.rect.y
+
