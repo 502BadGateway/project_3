6a6f5177b9b0e59de3f52af53d2fcdcdeb2c74d5
Adenuga Banjoko <banjokoa@uni.coventry.ac.uk>
2015-02-23 15:55:34 +0000

    Update robot.py

--- robot.py
+++ robot.py
@@ -1,26 +1,26 @@
 class Robot:
 
-	def __init__(self): #Constructor
-		pygame.sprite.Sprite.__init__(self)
-		if Robot.__image is none:             #What is this meant to be doing?
-			Robot.__image = pygame.image.load("robot.png")
+    def __init__(self): #Constructor
+	pygame.sprite.Sprite.__init__(self)
+	if Robot.__image is none:             #What is this meant to be doing?
+		Robot.__image = pygame.image.load("robot.png")
 
-		self.__image = Robot.image
-		self.__name = ""
-		self.__points = ""
-		self.__rect = Robot.__image.get_rect()
-		self.__rect.x = 0
-		self.__rect.y = 0
+	self.__image = Robot.image
+	self.__name = ""
+	self.__points = ""
+	self.__rect = Robot.__image.get_rect()
+	self.__rect.x = 0
+	self.__rect.y = 0
 
 
     def moveUp(self):   #move robot functions
-		
+        print "up"
     def moveDown(self):  #move robot functions  
-
+        print "Down"
     def moveLeft(self):  #move robot functions 
-
+        print "left"
     def moveRight(self):  #move robot functions
-
+        print "Right"
     def returnLocationX():  #Return the X location of the bot
         return self.__rect.x
 
@@ -29,6 +29,3 @@ class Robot:
 
     def returnPoints(self):
         return self.__points
-
-
-
