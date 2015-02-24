ab56510e7612f05f0da1e36dd3eec1d79acc7529
samathy <milktheelephant@gmail.com>
2015-02-24 14:27:49 +0000

    Renamed variables with correct name

--- collectorBot.py
+++ collectorBot.py
@@ -12,23 +12,23 @@ class collectorBot(robot):      #Class for the collector robot. Inherits from th
     def treasureCheck(self, arena, treasureLandmark1, treasureLandmark2, treasureLandmark3, treasureLandmark4, treasureLandmark5):
       if self.ret_element_val(self.locationX,self.locationY) == treasureLandmark1: 
         print "Treasure in London Found!"
-        self.points += 1 #this adds a point to robot's score
+        self.__points += 1 #this adds a point to robot's score
 
       elif self.ret_element_val(self.locationX,self.locationY) == treasureLandmark2: 
         print "Treasure in Paris Found!"
-        self.points += 1
+        self.__points += 1
 
       elif self.ret_element_val(self.locationX,self.locationY) == treasureLandmark3: 
         print "Treasure in Johnasaburgg Found!"
-        self.points += 1
+        self.__points += 1
           
       elif self.ret_element_val(self.locationX,self.locationY) == treasureLandmark4: 
         print "Treasure in Tokyo Found!"
-        self.points += 1      
+        self.__points += 1      
           
       elif self.ret_element_val(self.locationX,self.locationY) == treasureLandmark5: 
         print "Treasure in New York Found!"
-        self.points += 1
+        self.__points += 1
       else:
         print "There is no Treasrue at this landmark"
         
