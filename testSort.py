2f98749840e55d57419264bedb98799654685a6c
Dan Beglin <dbeglinuk@gmail.com>
2015-02-25 11:42:36 +0000

    Fixed display

--- testSort.py
+++ testSort.py
@@ -6,63 +6,54 @@ However, we are over a week away from completion.
 This, when completed will allow the group to test their own sorting algorithms
 
 """
-
+import sys
 import pygame
+from pygame.locals import *
 
 pygame.init()
 
-class display:
+screen = pygame.display.set_mode((1280,720))
+pygame.display.set_caption("Sort Bot")
+background = pygame.image.load('ASSETS/sortBackground.png')
+screen.blit(background, (0,0))
+class sortBot:
 
 	def __init__(self):
 
-
-		self.displaySort = pygame.display.set_mode((1280, 720))
-		self.background = pygame.image.load(sortBackground)
-		self.size = self.background.get_size()
-
-	def updateRobot(self):
-
-		self.display.blit(sortBot.image, (sortBot.rect.x, sortBot.rect.y)) #Blits robot image to screen
-
-
-
-
-class sortBot:
-
-	def __init__(self, Sprite):
-		pygame.sprite.Sprite.__init__(self, Sprite)
-		if sortBot.image is None:             #Loads image dispite some sort of pygame feature to do with Sprites.
-			sortBot.image = pygame.image.load("robot.png")
-
-		self.image = sortBot.image
 		self.name = ""
-		self.points = ""
+		self.points = 0
+		self.targetX = 0
+		self.targetY = 0
+		self.image = pygame.image.load('ASSETS/chuckle-1.png')
 		self.rect = self.image.get_rect()
 		self.rect.x = 0
 		self.rect.y = 0
-		self.targetX = 0
-		self.targetY = 0
 
-	def returnRectX(self):
-		return self.rect.x
+	def updateRobot(self):
 
-	def returnRectY(self):
-		return self.rect.y
+		self.image = pygame.transform.scale(self.image, (120,200))
+		screen.blit(self.image, (self.rect.x, self.rect.y))
+		print "BLIT"
 
 
-barry = sortBot(pygame.sprite.Sprite)
-barry.name = "Barry"
-barry.points = 0
-self.rect.x = 100
-self.rect.y = 100
+theSortBot = sortBot()
+theSortBot.name = "Fred"
+theSortBot.rect.x = 150
+theSortBot.rect.y = 500
 
-"""
+treasureList = []
 
- #For Later
 
-	def moveToTarget(self):
+while 1:
+	for event in pygame.event.get():
+		if event.type == QUIT:
+			pygame.quit()
+			sys.exit()
 
-		if(self.rect.x > self.targetX): #If robot is currently 
-			self.rect.x = self.rect.x - 1 
+	theSortBot.updateRobot()	
 
-"""
+	pygame.display.update()		
+
+
+
+
