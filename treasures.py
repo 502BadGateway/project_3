ba7c28327877023887c7296e7e8bcc98501a57ab
Dan Beglin <dbeglinuk@gmail.com>
2015-02-25 18:47:06 +0000

    explained by self in a comment essay

--- treasures.py
+++ treasures.py
@@ -4,4 +4,10 @@
 
 class treasure(entity):
 	def __init__(self):
-		self.sortListLocation = 0
+		self.sortListLocation = 0 
+		#This is for the sortBot to know where it is on inventory list.
+		#You need to change this var after you have called swap(n,m)
+		#to the new location of the file.
+		#maybe I will add that to the swap function. 
+		#ask me, i am thiking in code areas.
+
