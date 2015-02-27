3859c944d975b2fb72c3048511a3f58aaa647e4b
Sam Barratt <milktheelephant@gmail.com>
2015-02-27 19:24:40 +0000

    Further additions

--- city.py
+++ city.py
@@ -4,7 +4,7 @@ class city():
 
 	def __init__(self, cityName):
 		
-		self.__name = ""
+		self.__name = cityName 
 		self.__arena = arena(cityname)   #Create a new instance of an arena in the city class.
 		self.__image = self.arena.ret_image() 
 		
