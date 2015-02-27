c9cc978550cd26d453cd8e55ae635ca366e85967
Sam Barratt <milktheelephant@gmail.com>
2015-02-27 17:23:39 +0000

    streamlined treasure checking process

--- collectorBot.py
+++ collectorBot.py
@@ -10,34 +10,42 @@ import time
 class collectorBot(robot):      #Class for the collector robot. Inherits from the super class robot.
     def __init__(self, arena, wishList, treasureList):
     
-        self.__wishlist = [] #Define with wishlist and inventory varS
+        self.__wishlist = set(wishlist)#Define with wishlist and inventory varS
         self.__inventory = [] 
         self.__treasureList = treasureList 
     
     def treasureCheck(self, arena): #checks for treasures around this location. TODO Should make sure that Phil knows what order this funtion expects the treasures in.
-      
-      if self.ret_element_val(self.locationX,self.locationY) == treasureLandmark[0]: 
-        print "Treasure in London Found!"
-        self.__points += 1 #this adds a point to robot's score if it comes across this treasure
 
-      elif arena.ret_element_val(self.locationX,self.locationY) == treasureLandmark[1]: 
-        print "Treasure in Paris Found!"
-        self.__points += 1
 
-      elif arena.ret_element_val(self.locationX,self.locationY) == treasureLandmark[2]: 
-        print "Treasure in Johnasaburgg Found!"
-        self.__points += 1
-          
-      elif arena.ret_element_val(self.locationX,self.locationY) == treasureLandmark[3]: 
-        print "Treasure in Tokyo Found!"
-        self.__points += 1      
-          
-      elif arena.ret_element_val(self.locationX,self.locationY) == treasureLandmark[4]: 
-        print "Treasure in New York Found!"
-        self.__points += 1
-      else:
-        print "There is no Treasrue at this landmark"
+        for treasure in self.__treasureList:        #For every item  in the list of treasures
+            if arena.ret_element_val(self.locationX, self.locationY) == treasure.getArenaVal(): #Check if we're on top of them
+                if treasure.getArenaValue in self.wishlist: #If we are, then see if they're in our wishlist
+                    self.__inventory.append(treasure)       #If so then add them to the inventory
+                    self.__points += 1                      #And add to the score
         return
+
+      
+#      if self.ret_element_val(self.locationX,self.locationY) == treasureLandmark[0]: 
+#        print "Treasure in London Found!"
+#        self.__points += 1 #this adds a point to robot's score if it comes across this treasure
+#
+#      elif arena.ret_element_val(self.locationX,self.locationY) == treasureLandmark[1]: 
+#        print "Treasure in Paris Found!"
+#        self.__points += 1
+#
+#      elif arena.ret_element_val(self.locationX,self.locationY) == treasureLandmark[2]: 
+#        print "Treasure in Johnasaburgg Found!"
+#        self.__points += 1
+#          
+#      elif arena.ret_element_val(self.locationX,self.locationY) == treasureLandmark[3]: 
+#        print "Treasure in Tokyo Found!"
+#        self.__points += 1      
+#          
+#      elif arena.ret_element_val(self.locationX,self.locationY) == treasureLandmark[4]: 
+#        print "Treasure in New York Found!"
+#        self.__points += 1
+#      else:
+#        print "There is no Treasrue at this landmark"
         
     def trapCheck(self, arena, trapLandmark6): #this is a trap check which works in a similar way as the treasure check
       if arena.ret_element_val(self.locationX,self.locationY) == trapLandmark6:
