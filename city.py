07f75d8706f9fc1069e95353d6bba6af6fca42c1
Sam Barratt <milktheelephant@gmail.com>
2015-02-27 19:15:35 +0000

    Intigrated instances of arena into city superclass

--- city.py
+++ city.py
@@ -1,8 +1,10 @@
+import gen_arena
+
 class city():
 
-	def __init__():
+	def __init__(self, cityName):
 		
 		self.name = ""
-		self.arena = ""
-		self.image = ""
+		self.arena = arena(cityname)   #Create a new instance of an arena in the city class.
+		self.image = self.arena.ret_image() 
 		
