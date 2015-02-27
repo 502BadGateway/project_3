dcf593c3929e486f9b6894072b262de3e763029c
Sam Barratt <milktheelephant@gmail.com>
2015-02-27 16:05:07 +0000

    Added TODOs in comments

--- treasures.py
+++ treasures.py
@@ -8,6 +8,8 @@ class treasure(pygame.sprite.Sprite, entity): #I mocked up a treasure class, we 
         entity.__init__(self, name, val) #Call the base class constructor. This is because this constructor should be extending not replacing functionality in entity._init__. 
                                          #See here for information on this: Paragraph 6  https://docs.python.org/2/tutorial/classes.html#inheritance
 
+        #TODO MAKE THE MEMBER VARIABLES PRIVATE BY ADDING __ TO THE START OF THE VARIABLE NAME
+        #TODO ONCE THE ABOVE IS DONE, ADD GETTER METHODS FOR ALL RELEVENT VARIABLES THAT MIGHT BE NEEDED TO BE RETREIVED FROM OUTSIDE THIS CLASS.
 		pygame.sprite.Sprite.__init__(self) #no idea why i need it, but i need it
 		self.name = "" #give that dubbloon a name
 		self.image = pygame.image.load('ASSETS/testTreasure.png') #and a picture
