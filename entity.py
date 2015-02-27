b7db439bdfacc89446f0bc368b91535317a1cb18
Sam Barratt <milktheelephant@gmail.com>
2015-02-27 15:49:23 +0000

    Added member variables

--- entity.py
+++ entity.py
@@ -1,6 +1,7 @@
 #Entity class. Should be VERY similar to landmarks class from last project
 
 class entity:
-	def __init__(self):
+	def __init__(self, name, val):
 		self.name = ""
+        self.__arenaValue = val #Value representing this object in the arena
 		
