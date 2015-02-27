6e548f1039784bf60659fb9973e1d7b577fd4c35
Sam Barratt <milktheelephant@gmail.com>
2015-02-27 19:16:00 +0000

    Made variables private

--- city.py
+++ city.py
@@ -4,7 +4,7 @@ class city():
 
 	def __init__(self, cityName):
 		
-		self.name = ""
-		self.arena = arena(cityname)   #Create a new instance of an arena in the city class.
-		self.image = self.arena.ret_image() 
+		self.__name = ""
+		self.__arena = arena(cityname)   #Create a new instance of an arena in the city class.
+		self.__image = self.arena.ret_image() 
 		
