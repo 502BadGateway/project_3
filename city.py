f8e10c95b008f383dab0f4ef4edd69baaa6f873c
Sam Barratt <milktheelephant@gmail.com>
2015-02-27 19:57:23 +0000

    Added treasure listvariable and updated arena creation call

--- city.py
+++ city.py
@@ -2,9 +2,9 @@ import gen_arena
 
 class city():
 
-	def __init__(self, cityName):
+	def __init__(self, cityName, treasuresList):
 		
 		self.__name = cityName 
-		self.__arena = arena(cityname)   #Create a new instance of an arena in the city class.
+		self.__arena = arena(cityname, treasureList)   #Create a new instance of an arena in the city class.
 		self.__image = self.arena.ret_image() 
 		
