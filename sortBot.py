a063a6ac10be01d2ea989f2af5bea155a6a42656
Dan Beglin <dbeglinuk@gmail.com>
2015-02-28 10:00:21 +0000

    added moveLeft function

--- sortBot.py
+++ sortBot.py
@@ -17,5 +17,30 @@ class SortBot(robot, pygame.sprite.Sprite): #class called sortbot.
 		self.loaded = False #is the robot crrying a treasure?
 		self.target = 0 #where on the list is the robot aimed?
 
+	def moveLeft(self):
+		if(self.loaded == False):
+			#for i in range(0,28): #moves 140 pixels
+			self.rect.x -= 5 #left 2 pixels
+				#screen.blit(background, (0, 0)) #makes the background blit
+				#self.update() #updates something else
+				#for i in treasureList: #goes through the treasures argh
+					#i.update() #updates that too
+				#pygame.display.update() #updates the screen
+				#time.sleep(0.001) #go to sleep pygame
+			self.location = self.location - 1 #change the array location
+		elif(self.loaded == True):
+			#for i in range(0,28): #moves 140 pixels
+			self.rect.x -= 5 #left 2 pixels
+			self.carrying.rect.x = self.rect.x
+			self.carrying.rect.y = self.rect.y
+				#screen.blit(background, (0, 0)) #makes the background blit
+				#self.update() #updates something else
+				#self.carrying.update()
+				#for i in treasureList: #goes through the treasures argh
+				#	i.update() #updates that too
+				#pygame.display.update() #updates the screen
+				#time.sleep(0.001) #go to sleep pygame
+			self.location = self.location - 1 #change the array location
+
 
 		
