305a4f212b7b8d6a6a9307117d98c272861f149d
Dan Beglin <dbeglinuk@gmail.com>
2015-02-28 10:15:55 +0000

    added null incase i need it

---
+++ null.py
@@ -0,0 +1,4 @@
+class Null(treasure): #nt sure if i will need this when we intergreate
+
+	def update(self): #but it works, when this is in the list, we dont bug out when we
+		return None #run i.update()
