e38cae712789f871354eaf19293e16dc07d16811
samathy <milktheelephant@gmail.com>
2015-03-02 15:51:34 +0000

    Made arena instance public

--- city.py
+++ city.py
@@ -5,8 +5,8 @@ class city():
 	def __init__(self, cityName, treasuresList):
 		
 		self.__name = cityName 
-		self.__arena = arena(self.__name, treasuresList)   #Create a new instance of an arena in the city class.
-		self.__image = self.__arena.ret_image(self.__arena) 
+		self.arena = arena(self.__name, treasuresList)   #Create a new instance of an arena in the city class.
+		self.__image = self.arena.ret_image() 
                 print "City setup finished"
 
 		
