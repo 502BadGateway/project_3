4191f78d5614b5dd778550271a14fa2940e23ad3
samathy <milktheelephant@gmail.com>
2015-03-03 14:28:06 +0000

    Removed Git Merge conflict tags

--- sort.py
+++ sort.py
@@ -4,12 +4,6 @@ class Sorter:
 
     def __init__(self):
 
-
-<<<<<<< HEAD
-<<<<<<< HEAD #this heck is this? 
-			 #this is all wrong. erasing it
-=======
->>>>>>> f8e10c95b008f383dab0f4ef4edd69baaa6f873c
     def swap(self, valOne, valTwo, lst):
         tmp = None  
 
