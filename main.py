1ec68d94be923e672748af15e1fc29882f7a8308
Sam Barratt <milktheelephant@gmail.com>
2015-03-03 22:04:36 +0000

    Oops, made the wrong correction, should be right now

--- main.py
+++ main.py
@@ -263,7 +263,7 @@ def main(mapSelect):
 				if (city.arena.ret_element_value(x,y) == 1 or city.arena.ret_element_value(x,y) == 2):
 					treasurePosListX[i] = x #puts location in 2 lists for reference later if needed
 					treasurePosListY[i] = y
-					treasureNum = city.arena.ret_element_value(x,y)  #changes the number in the array from a road to the relevant treasure number the treasures will always be placed in the same order, the order they are in arrayNumberReference.txt
+					city.arena.put(x,y, treasureNum)  #changes the number in the array from a road to the relevant treasure number the treasures will always be placed in the same order, the order they are in arrayNumberReference.txt
 					treasureNum = treasureNum + 1 #im want to reserve numbers 5 to 19 for treasures 
 					i=i+1
 				else:
