86d4c5d2345444bc956756843e8826c79ac584bc
femibak96 <femz300@gmail.com>
2015-03-03 14:33:32 +0000

    Create sortSelection.py

---
+++ sortSelection.py
@@ -0,0 +1,12 @@
+import pygame
+class mapSelect():
+	def __init__(self,sortName,buttonX,buttonY,imageLocation,buttonHeight,buttonWidth):
+		self.sortName = sortName
+		self.buttonX = buttonX
+		self.buttonY = buttonY
+		self.imageLocation = imageLocation
+		self.buttonHeight = buttonHeight
+		self.buttonWidth = buttonWidth		
+
+
+
