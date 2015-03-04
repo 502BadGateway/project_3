2e795635c024c792502ee8e362be58eaae4097c5
Sam Barratt <milktheelephant@gmail.com>
2015-03-04 00:13:42 +0000

    Apparently i changed something...

--- entity.py
+++ entity.py
@@ -1,7 +1,7 @@
 #Entity class. Should be VERY similar to landmarks class from last project
 
 class entity:
-	def __init__(self, name, val, image):
+    def __init__(self, name, val, image):
         self.__name = name
         self.__arenaValue = val #Value representing this object in the arena
         self.__image = image
