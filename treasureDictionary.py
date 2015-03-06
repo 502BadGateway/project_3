44e8202f0a3ea3350addbc3b6149b8564c2b01d1
Dan Beglin <dbeglinuk@gmail.com>
2015-03-06 12:33:31 +0000

    changed dictionary

--- treasureDictionary.py
+++ treasureDictionary.py
@@ -2,18 +2,18 @@ class treasureDictionary:
 
 	def __init__(self):
 
-		self.chest = {"points" : 10, "arenaVal": 5}
-		self.coin = {"points" : 20, "arenaVal": 6}
-		self.crown = {"points": 15, "arenaVal": 7}
-		self.diamond = {"points" : 9, "arenaVal": 8}
-		self.diamondBlock = {"points" : 81, "arenaVal": 9}
-		self.emerald = {"points": 11, "arenaVal": 10}
-		self.emeraldBlock = {"points": 99, "arenaVal": 11}
-		self.goldbar = {"points": 8, "arenaVal": 12}
-		self.goldBlock = {"points": 64, "arenaVal": 13}
-		self.iron = {"points": 7, "arenaVal": 14}
-		self.lapis = {"points": 4, "arenaVal": 15}
-		self.lapisBlock = {"points": 36, "arenaVal": 16}
-		self.ring = {"points": 30, "arenaVal": 17}
-		self.sword = {"points": 40, "arenaVal": 18}
-		self.tiara = {"points" : 100, "arenaVal": 19}
+		self.chest = {"points" : 10, "arenaVal": 5, "image": 'ASSETS/treasures/chest.png'}
+		self.coin = {"points" : 20, "arenaVal": 6, "image": 'ASSETS/treasures/coin.png'}
+		self.crown = {"points": 15, "arenaVal": 7, "image": 'ASSETS/treasures/crown.png'}
+		self.diamond = {"points" : 9, "arenaVal": 8, "image": 'ASSETS/treasures/diamond.png'}
+		self.diamondBlock = {"points" : 81, "arenaVal": 9, "image": 'ASSETS/treasures/diamondblock.png'}
+		self.emerald = {"points": 11, "arenaVal": 10, "image": 'ASSETS/treasures/emerald.png'}
+		self.emeraldBlock = {"points": 99, "arenaVal": 11, "image": 'ASSETS/treasures/emeraldblock.png'}
+		self.goldbar = {"points": 8, "arenaVal": 12, "image": 'ASSETS/treasures/goldbar.png'}
+		self.goldBlock = {"points": 64, "arenaVal": 13, "image": 'ASSETS/treasures/goldblock.png'}
+		self.iron = {"points": 7, "arenaVal": 14, "image": 'ASSETS/treasures/iron.png'}
+		self.lapis = {"points": 4, "arenaVal": 15, "image": 'ASSETS/treasures/lapis.png'}
+		self.lapisBlock = {"points": 36, "arenaVal": 16, "image": 'ASSETS/treasures/lapisblock.png'}
+		self.ring = {"points": 30, "arenaVal": 17, "image": 'ASSETS/treasures/ring.png'}
+		self.sword = {"points": 40, "arenaVal": 18, "image": 'ASSETS/treasures/sword.png'}
+		self.tiara = {"points" : 100, "arenaVal": 19, "image": 'ASSETS/treasures/tiara.png'}
