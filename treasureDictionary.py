8b66bd8fd6484e2525d679641c4defdd71f877c4
Dan Beglin <dbeglinuk@gmail.com>
2015-03-06 15:23:06 +0000

    got rid of these STUPID STIPUD caps

--- treasureDictionary.py
+++ treasureDictionary.py
@@ -6,7 +6,7 @@ class dictionary:
 		self.coin = {"points" : 20, "arenaVal": 6, "image": 'ASSETS/treasures/coin.png'}
 		self.crown = {"points": 15, "arenaVal": 7, "image": 'ASSETS/treasures/crown.png'}
 		self.diamond = {"points" : 9, "arenaVal": 8, "image": 'ASSETS/treasures/diamond.png'}
-		self.diamondBlock = {"points" : 81, "arenaVal": 9, "image": 'ASSETS/treasures/diamondblock.png'}
+		self.diamondblock = {"points" : 81, "arenaVal": 9, "image": 'ASSETS/treasures/diamondblock.png'}
 		self.emerald = {"points": 11, "arenaVal": 10, "image": 'ASSETS/treasures/emerald.png'}
 		self.emeraldblock = {"points": 99, "arenaVal": 11, "image": 'ASSETS/treasures/emeraldblock.png'}
 		self.goldbar = {"points": 8, "arenaVal": 12, "image": 'ASSETS/treasures/goldbar.png'}
